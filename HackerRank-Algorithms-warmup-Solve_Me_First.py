c0c4f3a918f8b375e0916d4a87f638d0df27c85f
Hariprasath G <hariprasath167@gmail.com>
2022-03-25 13:48:16 +0530

    HackerRank-Algorithms-warmup-Solve_Me_First

---
+++ HackerRank-Algorithms-warmup-Solve_Me_First.py
@@ -0,0 +1,10 @@
+# HackerRank-Algorithms-warmup-Solve_Me_First
+
+def solveMeFirst(a,b):
+    if a <= 1000 and b <= 1000:
+        return a+b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
